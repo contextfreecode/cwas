016b536610463211fbef8cf63d8751f0202f65f3
Tom <tjpalmer@pop-os.localdomain>
2020-10-12 20:27:47 -0700

    Vastly improve bash

--- census.bash
+++ census.bash
@@ -1,26 +1,16 @@
+cancel=2
+
 trap '' INT
 
-cancel=2
-success=0
-
 while true; do
-    # Name
-    # name=$(./field Name 50000000000)
-    name=$(./field Name 5)
-    code=$?
-    if [ $code -eq $cancel ]; then
-        continue;
-    elif [ $code -ne $success ]; then
-        break;
-    fi
-    # Age
-    age=$(./constrained_field Age 3 0123456789)
-    code=$?
-    if [ $code -eq $cancel ]; then
-        continue;
-    elif [ $code -ne $success ]; then
-        break;
-    fi
-    # Message
+    # name=$(./field Name 50000000000) || case $? in
+    name=$(./field Name 5) || case $? in
+        $cancel) continue;;
+        *) break;;
+    esac
+    age=$(./constrained_field Age 3 0123456789) || case $? in
+        $cancel) continue;;
+        *) break;;
+    esac
     echo $name is $age years old
 done
